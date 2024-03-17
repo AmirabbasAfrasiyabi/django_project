f2e67cfb7a7a1ad42af2aa7f96c0fb4177f3a319
Amirabbas <abb.afrasiyabi@gmail.com>
2024-03-18 00:06:09 +0330

    final update from this branch

--- core/pages/models.py
+++ core/pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    author = models.ForeignKey('users.User' ,  on_delete=models.CASCADE , related_name='articles')
+    creat_at = models.DateTimeField(auto_now_add = True)
+    category = models.ForeignKey('Category' , on_delete=models.PROTECT , related_name='articles')
+
+class Category(models.Model):
+    title= models.CharField(max_length=50) 
