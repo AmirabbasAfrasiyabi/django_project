2e99e90db78073acc34ad19a55e494ca30a282ef
Amirabbas <abb.afrasiyabi@gmail.com>
2024-03-17 23:37:45 +0330

    created model and app user

--- core/users/urls.py
+++ core/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-# from users.views import SignUpView
+from users.views import SignUpView
 
-# urlpatterns = [
-#     path('sign-up', SignUpView.as_view(), name='sign_up')
-# ]
+urlpatterns = [
+    path('sign-up', SignUpView.as_view(), name='sign_up')
+]
