baff12d0fd9d4d0f0241ed6cb947da3e79b78aae
Amirabbas <abb.afrasiyabi@gmail.com>
2024-03-17 23:28:22 +0330

    view changes for signup

--- core/users/views.py
+++ core/users/views.py
@@ -1,9 +1,11 @@
 
 from django.views.generic import CreateView 
 from users.forms import UserRegisterForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 class SignupView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/sign_up.html'
+    success_url = reverse_lazy('sign_up')   
